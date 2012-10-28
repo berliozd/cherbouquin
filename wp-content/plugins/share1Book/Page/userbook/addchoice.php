faad4b6f2a77ad1d9219d1c7fc8eb47f23cb18b7
Didier <Didier@ELY3STP03.ngtravel.grp>
2012-10-28 21:59:09 +0100

    #78 : ajout des commentaires sur la page userbook/addchoice

--- wp-content/plugins/share1Book/Page/userbook/addchoice.php
+++ wp-content/plugins/share1Book/Page/userbook/addchoice.php
@@ -1,5 +1,20 @@
 <?php
+
 use \Sb\Entity\Constants;
+use \Sb\Entity\LibraryPages;
+use \Sb\Entity\Urls;
+use \Sb\Db\Dao\BookDao;
+use \Sb\Db\Dao\UserBookDao;
+use \Sb\Db\Mapping\BookMapper;
+use \Sb\Flash\Flash;
+use \Sb\Db\Model\UserBook;
+use \Sb\Db\Model\Book;
+use \Sb\View\Book as BookView;
+use \Sb\Form\Book as BookForm;
+use \Sb\Helpers\HTTPHelper;
+use \Sb\Templates\Template;
+use \Sb\View\Components\ButtonsBar;
+use \Sb\Cache\ZendFileCache;
 
 global $s1b;
 $context = $s1b->getContext();
@@ -11,30 +26,35 @@ if ($context->getIsShowingFriendLibrary()) {
 
 if (!$s1b->getIsSubmit()) {
     // Récupération du Book depuis le cache
-    $book = \Sb\Cache\ZendFileCache::getInstance()->load(Constants::BOOK_TO_ADD_PREFIX . session_id());
+    $book = ZendFileCache::getInstance()->load(Constants::BOOK_TO_ADD_PREFIX . session_id());
+    
+    // If id is known, getting the book from db to have all associated members and userbooks to show the potential reviews
+    if ($book->getId())
+        $book = BookDao::getInstance()->get($book->getId());
+    
     showBookDetail($book);
 } else {
 
-    $bookForm = new \Sb\Form\Book($_POST);
+    $bookForm = new BookForm($_POST);
 
     // testing if book can be found in db by id
     if ($bookForm->getId())
-        $book = \Sb\Db\Dao\BookDao::getInstance()->get($bookForm->getId());
+        $book = BookDao::getInstance()->get($bookForm->getId());
 
     // testing if book can be found in db by isbn10, isbn13, asin
     if (!$book)
-        $book = \Sb\Db\Dao\BookDao::getInstance()->getOneByCodes($bookForm->getISBN10(), $bookForm->getISBN13(), $bookForm->getASIN());
+        $book = BookDao::getInstance()->getOneByCodes($bookForm->getISBN10(), $bookForm->getISBN13(), $bookForm->getASIN());
 
     // getting the book data from post and adding to db
     if (!$book) {
         // Récupération du Book depuis le POST
-        $book = new \Sb\Db\Model\Book();
+        $book = new Book();
         //var_dump($_POST);
-        \Sb\Db\Mapping\BookMapper::map($book, $_POST, "book_");
+        BookMapper::map($book, $_POST, "book_");
         // book not in db : need to add it
         $book->setCreationDate(new \DateTime());
         $book->setLastModificationDate(new \DateTime());
-        \Sb\Db\Dao\BookDao::getInstance()->add($book);
+        BookDao::getInstance()->add($book);
     }
 
     // Getting current user current nb userbooks in libary
@@ -42,13 +62,13 @@ if (!$s1b->getIsSubmit()) {
     $userNbUserBooks = count($user->getNotDeletedUserBooks());
 
     if ($userNbUserBooks >= $config->getMaximumNbUserBooksForPublic()) {
-        \Sb\Flash\Flash::addItem(sprintf(__("Vous ne pouvez pas avoir plus de %s livres dans votre bibliothèque.", "s1b"), $config->getMaximumNbUserBooksForPublic()));
+        Flash::addItem(sprintf(__("Vous ne pouvez pas avoir plus de %s livres dans votre bibliothèque.", "s1b"), $config->getMaximumNbUserBooksForPublic()));
     } else {
         // Ajout du UserBook
         if ($book) {
 
             $existingUserBook = false;
-            $userBook = \Sb\Db\Dao\UserBookDao::getInstance()->getByBookIdAndUserId($user->getId(), $book->getId());
+            $userBook = UserBookDao::getInstance()->getByBookIdAndUserId($user->getId(), $book->getId());
             // testing if the user already had the book but deleted it :
             // if yes, then the userbook is undeleted
             if ($userBook && $userBook->getIs_deleted()) {
@@ -56,7 +76,7 @@ if (!$s1b->getIsSubmit()) {
                 $existingUserBook = true;
             } else {
                 // Création du UserBoook
-                $userBook = new \Sb\Db\Model\UserBook();
+                $userBook = new UserBook();
                 $userBook->setCreationDate(new \DateTime);
             }
 
@@ -68,46 +88,35 @@ if (!$s1b->getIsSubmit()) {
 
             // persisting book in DB
             if ($existingUserBook) {
-                if (\Sb\Db\Dao\UserBookDao::getInstance()->update($userBook)) {
-                    \Sb\Flash\Flash::addItem(sprintf(__("Vous aviez déjà le livre '%s' dans votre bibliothèque mais l'aviez supprimé. Il a été rajouté.", "s1b"), $book->getTitle()));
-                    \Sb\Flash\Flash::addItem(__("Vous pouvez maintenant finir de remplir votre fiche de lecture.", "s1b"));
+                if (UserBookDao::getInstance()->update($userBook)) {
+                    Flash::addItem(sprintf(__("Vous aviez déjà le livre '%s' dans votre bibliothèque mais l'aviez supprimé. Il a été rajouté.", "s1b"), $book->getTitle()));
+                    Flash::addItem(__("Vous pouvez maintenant finir de remplir votre fiche de lecture.", "s1b"));
                 }
             } else {
-                if (\Sb\Db\Dao\UserBookDao::getInstance()->add($userBook)) {
-                    \Sb\Flash\Flash::addItem(sprintf(__("Le livre '%s' a été ajouté à votre bibliothèque.", "s1b"), $book->getTitle()));
-                    \Sb\Flash\Flash::addItem(__("Vous pouvez maintenant finir de remplir votre fiche de lecture.", "s1b"));
+                if (UserBookDao::getInstance()->add($userBook)) {
+                    Flash::addItem(sprintf(__("Le livre '%s' a été ajouté à votre bibliothèque.", "s1b"), $book->getTitle()));
+                    Flash::addItem(__("Vous pouvez maintenant finir de remplir votre fiche de lecture.", "s1b"));
                 }
             }
         }
     }
 
-    \Sb\Helpers\HTTPHelper::redirect(\Sb\Entity\Urls::USER_LIBRARY_DETAIL, array("page" => \Sb\Entity\LibraryPages::USERBOOK_EDIT, "ubid" => $userBook->getId()));
-
-    //\Sb\Helpers\HTTPHelper::redirect(\Sb\Entity\Urls::USER_LIBRARY_DETAIL, array("page" => \Sb\Entity\LibraryPages::USERBOOK_ADD));
+    HTTPHelper::redirect(Urls::USER_LIBRARY_DETAIL, array("page" => LibraryPages::USERBOOK_EDIT, "ubid" => $userBook->getId()));
 }
 
-//////////////////////////////////////////////////////
-//function showBookDetail(\Sb\Db\Model\Book $book) {
-//
-//    $bookView = new \Sb\View\Book($book, true, false, true);
-//    $buttonsBar = new \Sb\View\Components\ButtonsBar(false);
-//
-//    echo $bookView->get() . $buttonsBar->get();
-//}
-
-function showBookDetail(\Sb\Db\Model\Book $book) {
+function showBookDetail(Book $book) {
 
     // Préparation du template
-    $tpl = new \Sb\Templates\Template("userBook");
+    $tpl = new Template("userBook");
 
     $tpl->set("action", "");
 
-    $bookView = new \Sb\View\Book($book, true, false, true);
+    $bookView = new BookView($book, true, true, true);
     $tpl->set("book", $bookView->get());
 
     $tpl->set("bookForm", "");
 
-    $buttonsBar = new \Sb\View\Components\ButtonsBar(false);
+    $buttonsBar = new ButtonsBar(false);
     $tpl->set("buttonsBar", $buttonsBar->get());
 
     echo $tpl->output();
