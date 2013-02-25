3ee2ddf46b8df9fef504535fc583591cab0df12b
Didier <Didier@ELY3STP03.ngtravel.grp>
2013-02-25 20:53:54 +0100

    Added content type utf-8 in bootstrap

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -5,33 +5,18 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {
     protected function _initRouter() {
 
         $front = $this->bootstrap('FrontController')->getResource('FrontController');
-        $router = $front->getRouter();
+        //$router = $front->getRouter();
         $config = new Zend_Config_Ini(APPLICATION_PATH . '/configs/application.ini', 'production');
         $routing = new Zend_Controller_Router_Rewrite();
         $routing->addConfig($config, 'routes');
         $front->setRouter($routing);
     }
 
-    protected function _initZFDebug() {
-        // Setup autoloader with namespace
-        $autoloader = Zend_Loader_Autoloader::getInstance();
-        $autoloader->registerNamespace('ZFDebug');
+    protected function _initFrontControllerOutput() {
 
-        // Ensure the front controller is initialized
-        $this->bootstrap('FrontController');
-
-        // Retrieve the front controller from the bootstrap registry
-        $front = $this->getResource('FrontController');
-
-        // Only enable zfdebug if options have been specified for it
-        if ($this->hasOption('zfdebug')) {
-            // Create ZFDebug instance
-            $zfdebug = new
-                    ZFDebug_Controller_Plugin_Debug($this->getOption('zfdebug'));
-
-            // Register ZFDebug with the front controller
-            $front->registerPlugin($zfdebug);
-        }
+        $front = $this->bootstrap('FrontController')->getResource('FrontController');
+        $response = new Zend_Controller_Response_Http;
+        $response->setHeader('Content-Type', 'text/html; charset=UTF-8', true);
+        $front->setResponse($response);
     }
-
 }
