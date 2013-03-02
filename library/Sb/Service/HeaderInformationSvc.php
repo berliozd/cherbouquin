03d778660ff7567f12deece09bcd9ddbad9c1777
Didier <Didier@ELY3STP03.ngtravel.grp>
2013-03-02 01:14:32 +0100

    Updated header informaion for all static pages

--- library/Sb/Service/HeaderInformationSvc.php
+++ library/Sb/Service/HeaderInformationSvc.php
@@ -143,12 +143,46 @@ class HeaderInformationSvc extends Service {
             $result = new HeaderInformation;
 
             switch ($routeName) {
+                case "newsletters":
+                    $result->setTitle(sprintf(__("%s : newsletter", "s1b"), Constants::SITENAME));
+                    $result->setDescription("Les newsletters envoyées pour vous tenir informé des parutions, des coups de cœur et commentaires de la communauté");
+                    $result->setKeywords("newsletter");
+                    break;
+                case "pressReviews":
+                    $result->setTitle(sprintf(__("%s : que s'est-il passé dans la presse ?", "s1b"), Constants::SITENAME));
+                    $result->setDescription("Les revues de presse envoyées pour vous tenir informé des derniers articles, interviews ou polémiques parus");
+                    $result->setKeywords("revue de presse");
+                    break;
+                case "stepByStep":
+                    $result->setTitle(sprintf(__("%s : aide - pas à pas - comment utiliser le site ?", "s1b"), Constants::SITENAME));
+                    $result->setDescription("Si vous avez besoin d'un pas à pas pour utiliser pleinement toutes les fonctionnalités du site");
+                    $result->setKeywords("aide|pas à pas");
+                    break;
                 case "about":
-                    $result->setTitle(sprintf(__("%s : à propos du projet","s1b"), Constants::SITENAME));
+                    $result->setTitle(sprintf(__("%s : à propos du projet", "s1b"), Constants::SITENAME));
+                    $result->setDescription("Comment et pourquoi nous est venu cette idée de communauté autour du livre et de la lecture");
+                    $result->setKeywords("à propos|projet|pourquoi|société");
+                    break;
+                case "press":
+                    $result->setTitle(sprintf(__("%s : la presse et les blogs littéraires parlent de nous", "s1b"), Constants::SITENAME));
+                    $result->setDescription("Envoi de communiqués de presse aux médias pour qu'ils parlent de nous | les blogs et blogueurs littéraires qui nous soutiennent");
+                    $result->setKeywords("presse|blog|blogueurs littéraires|blogueurs BD");
+                    break;
+                case "team":
+                    $result->setTitle(sprintf(__("%s : découvrir l'équipe - les collaborateurs", "s1b"), Constants::SITENAME));
                     $result->setDescription("Découvrez l'équipe qui travaille derrière un ordinateur, mais pas que, pour développer la communauté");
                     $result->setKeywords("équipe|collaborateur");
                     break;
-
+                case "howItWorks":
+                    $result->setTitle(sprintf(__("%s : aide - comment ça marche ?", "s1b"), Constants::SITENAME));
+                    $result->setDescription("Si vous souhaitez tout comprendre sur l'utilisation du site sans rentrer dans un pas à pas");
+                    $result->setKeywords("aide générale");
+                    break;
+                case "helpUs":
+                    $result->setTitle(sprintf(__("%s : pourquoi et comment nous aider ?", "s1b"), Constants::SITENAME));
+                    $result->setDescription("Si vous souhaitez nous donner un coup de pouce sans même vous en rendre compte");
+                    $result->setKeywords("nous aider");
+                    break;
                 default:
                     break;
             }
