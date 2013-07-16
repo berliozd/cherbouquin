6b6814160a22b84d35c658e6576811c47f501134
Didier <berliozd@gmail.com>
2013-07-16 23:14:42 +0200

    Added microdata to book reviews

--- library/Sb/Db/Model/Book.php
+++ library/Sb/Db/Model/Book.php
@@ -292,9 +292,9 @@ class Book implements \Sb\Db\Model\Model {
         }
         $publicationInfo = "";
         if ($this->getPublishingDate() && $pub) // publisher et date de publication renseignées
-            $publicationInfo = sprintf(__("Publié le <span itemprop=\"datePublished\" content=\"%s\">%s</span> par <span class=\"publisher\" itemprop=\"publisher\">%s</span>", "s1b"), $this->getPublishingDate()->format(__("Y-m-d", "s1b")), $pubDtStr, $pub);
+            $publicationInfo = sprintf(__("Publié le <meta itemprop=\"datePublished\" content=\"%s\" />%s par <span class=\"publisher\" itemprop=\"publisher\">%s</span>", "s1b"), $this->getPublishingDate()->format(__("Y-m-d", "s1b")), $pubDtStr, $pub);
         elseif ($this->getPublishingDate()) // date de publication renseignée
-            $publicationInfo = sprintf(__("Publié le <span itemprop=\"datePublished\" content=\"%s\">%s</span>", "s1b"), $this->getPublishingDate()->format(__("Y-m-d", "s1b")), $pubDtStr);
+            $publicationInfo = sprintf(__("Publié le <meta itemprop=\"datePublished\" content=\"%s\" />%s", "s1b"), $this->getPublishingDate()->format(__("Y-m-d", "s1b")), $pubDtStr);
         elseif ($pub) // publisher renseigné
             $publicationInfo = sprintf(__("Publié par <span class=\"publisher\" itemprop=\"publisher\">%s</span>", "s1b"), $pub);
 
