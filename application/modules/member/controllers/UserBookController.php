8d6d59cce57c19b191621bfdd14140624e9c4e73
Didier <berliozd@gmail.com>
2014-06-24 11:34:22 +0200

    Migrate import userbook to Zend

--- application/modules/member/controllers/UserBookController.php
+++ application/modules/member/controllers/UserBookController.php
@@ -309,4 +309,277 @@ class Member_UserBookController extends Zend_Controller_Action {
             $this->forward("error", "error", "default");
         }
     }
+
+    public function importAction() {
+
+        try {
+
+            $tpl = new \Sb\Templates\Template("import");
+            $tpl->set("summary", __("Merci de sélectionner un fichier au format CSV, le séparateur de colonne devant être \",\" et le code ISBN des livres sur 10 caractères devant apparaître dans la première colonne.", "share1book"));
+            echo $tpl->output();
+
+        } catch (\Exception $e) {
+            Trace::addItem(sprintf("Une erreur s'est produite dans \"%s->%s\", TRACE : %s\"", get_class(), __FUNCTION__, $e->getTraceAsString()));
+            $this->forward("error", "error", "default");
+        }
+    }
+
+    public function submitImportAction() {
+
+        try {
+
+            global $globalContext;
+            global $globalConfig;
+
+            $tpl = new \Sb\Templates\Template("import");
+
+            $isbnsInFile = $this->getIsbnsInFile($globalContext);
+
+            if ($isbnsInFile) {
+
+                if (count($isbnsInFile) > $globalConfig->getMaxImportNb()) {
+                    Flash::addItem(sprintf(__("Il n'est pas possible d'importer plus de %s livres.", "s1b"), $globalConfig->getMaxImportNb()));
+                } else {
+
+                    //\Sb\Flash\Flash::addItem(count($isbnsInFile) . " ont été soumis dans votre fichier d'import.");
+                    Flash::addItem(sprintf(__("%s livre(s) ont été soumis dans votre fichier d'import.", "s1b"), count($isbnsInFile)));
+
+                    /// RECHERCHE DES LIVRES (base et amazon)
+                    $resultingBooks = $this->searchBooks($isbnsInFile, $globalConfig);
+
+                    /// AJOUT DES LIVRES dans la base
+                    $booksAlReadyImported = array();
+                    $booksCorrectlyImported = array();
+                    $this->loadBooks($resultingBooks, $booksAlReadyImported, $booksCorrectlyImported, $globalContext);
+
+                    // MET A JOUR le résumé de l'import
+                    $this->updateSummary($isbnsInFile, $tpl, $booksAlReadyImported, $booksCorrectlyImported, $globalContext);
+                }
+            } else {
+                $tpl->set("summary", "");
+                Flash::addItem(__("Aucun ISBN n'a pu être lu dans le fichier soumit.", "s1b"));
+            }
+
+        } catch (\Exception $e) {
+            Trace::addItem(sprintf("Une erreur s'est produite dans \"%s->%s\", TRACE : %s\"", get_class(), __FUNCTION__, $e->getTraceAsString()));
+            $this->forward("error", "error", "default");
+        }
+    }
+
+    // Function for import action
+
+    private function updateSummary($isbnsInFile, &$tpl, $booksAlReadyImported, $booksCorrectlyImported, \Sb\Context\Model\Context $context) {
+        $alreadyImported = "";
+        if (count($booksAlReadyImported) > 0)
+            $alreadyImported = $this->showTableSummary(sprintf(__("%s livre(s) déjà présents dans votre bibliothèque", "s1b"),
+                count($booksAlReadyImported)), $booksAlReadyImported, $context);
+
+        $correctlyImported = "";
+        if (count($booksCorrectlyImported) > 0)
+            $correctlyImported = $this->showTableSummary(sprintf(__("%s livre(s) correctement ajoutés à votre bibliothèque", "s1b"),
+                count($booksCorrectlyImported)), $booksCorrectlyImported, $context);
+        $notImported = "";
+
+        $isbnCorrectlyImported = array_map(array(&$this, "getIsbnFromBook"), $booksCorrectlyImported);
+        $isbnAlreadyImported = array_map(array(&$this, "getIsbnFromBook"), $booksAlReadyImported);
+        $isbnOK = array_merge($isbnAlreadyImported, $isbnCorrectlyImported);
+        $isbnsNotImported = array_diff($isbnsInFile, $isbnOK);
+        if (count($isbnsNotImported) > 0)
+            $notImported = $this->showNotImported($isbnsNotImported, $context);
+
+
+        $tpl->set("summary", $correctlyImported . $alreadyImported . $notImported);
+    }
+
+    private function loadBooks($resultingBooks, &$booksAlReadyImported, &$booksCorrectlyImported, \Sb\Context\Model\Context $context) {
+
+        if ($resultingBooks) {
+
+            $bookDao = \Sb\Db\Dao\BookDao::getInstance();
+
+            foreach ($resultingBooks as $book) {
+
+                if (!$book->getId()) { // il faut créer le Book
+                    \Sb\Trace\Trace::addItem("Création du livre : " . $book->getISBN10() . "(" . $book->getTitle() . ")");
+                    $bookId = $bookDao->Add($book);
+                    if ($bookId) { // le Book a été créé correctement
+                        \Sb\Trace\Trace::addItem("Création OK");
+                        if ($this->addUserBook($bookId, $context)) {
+                            $booksCorrectlyImported[] = $book;
+                        }
+                    } else {
+                        \Sb\Trace\Trace::addItem("NOT Création OK");
+                    }
+                } else {
+                    // Le Book existe déjà, il y a des chances qui soit déjà affecté au user et il faut donc vérifier
+                    $userBookDao = \Sb\Db\Dao\UserBookDao::getInstance();
+                    $userBook = new \Sb\Db\Model\UserBook();
+                    $userBook = $userBookDao->getByBookIdAndUserId($context->getConnectedUser()->getId(), $book->getId());
+                    if ($userBook) { // le userBook existe déjà pour le user connecté
+                        \Sb\Trace\Trace::addItem("Le livre " . $book->getISBN10() . " - " . $book->getTitle() . " existe déjà pour cet utilisateur.");
+                        $booksAlReadyImported[] = $book;
+                    } else { // on peut créer le userBook
+                        if ($this->addUserBook($book->getId(), $context)) {
+                            $booksCorrectlyImported[] = $book;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private function searchBooks($isbnsInFile, Sb\Config\Model\Config $config) {
+
+        $booksInBase = $this->getBooksInBase($isbnsInFile);
+        \Sb\Trace\Trace::addItem(count($booksInBase) . " livres trouvés dans la Base.");
+
+        $resultingBooks = array();
+        if ($booksInBase) {
+            // récupération des books manquant dans amazon
+            if (count($booksInBase) != count($isbnsInFile)) {
+                $isbnsFromBase = array_map("getIsbnFromBook", $booksInBase);
+                //var_dump($isbnsFromBase);
+                $missedIsbns = array_diff($isbnsInFile, $isbnsFromBase);
+                \Sb\Trace\Trace::addItem(count($missedIsbns) . " livres à rechercher dans Amazon.");
+
+                $booksFromAmazon = $this->getBooksFromAmazon($missedIsbns, $config);
+                if ($booksFromAmazon) {
+                    \Sb\Trace\Trace::addItem(count($booksFromAmazon) . " livres trouvés dans Amazon.");
+                    $resultingBooks = array_merge($booksInBase, $booksFromAmazon);
+                    \Sb\Trace\Trace::addItem(count($resultingBooks) . " livres trouvés dans Amazon + Base.");
+                } else {
+                    $resultingBooks = $booksInBase;
+                }
+            } else {
+                $resultingBooks = $booksInBase;
+            }
+        } else { // aucun livre trouvé dans la base, rechercher dans Amazon
+            $resultingBooks = getBooksFromAmazon($isbnsInFile, $config);
+        }
+
+        return $resultingBooks;
+    }
+
+    private function getIsbnsInFile(\Sb\Context\Model\Context $context) {
+
+        $uploaddir = $context->getBaseDirectory() . "/var/uploads/";
+        $uploadfile = $uploaddir . session_id() . "_" . basename($_FILES['importFile']['name']);
+        if (move_uploaded_file($_FILES['importFile']['tmp_name'], $uploadfile)) {
+            \Sb\Trace\Trace::addItem("File is valid, and was successfully uploaded.");
+            $fileContent = file_get_contents($uploadfile);
+            $lines = explode("\r\n", $fileContent);
+            $isbns = array_filter(array_map(array(&$this, "getIsbnFromLine"), $lines), array(&$this, "isIsbn"));
+            // dédoublonage des isbns
+            $isbns = array_unique($isbns);
+            //var_dump($isbns);
+            return $isbns;
+        } else {
+            \Sb\Trace\Trace::addItem("Possible file upload attack!");
+        }
+    }
+
+    private function getIsbnFromLine($line) {
+        $cols = explode(",", $line);
+        return $cols[0];
+    }
+
+    private function getIsbnFromBook(\Sb\Db\Model\Book $book) {
+        return $book->getISBN10();
+    }
+
+    private function isIsbn($isbn) {
+        if (strlen($isbn) == 10) {
+            return true;
+        }
+        return false;
+    }
+
+    private function getBooksInBase($isbns) {
+        $bookDao = \Sb\Db\Dao\BookDao::getInstance();
+        return $bookDao->getOneByCodes($isbns, null, null);
+    }
+
+    private function getBooksFromAmazon($isbns, Config $config) {
+
+        \Sb\Trace\Trace::addItem(count($isbns) . " Isbns vont être requetés à Amazon : " . implode(",", $isbns));
+
+        $booksFromAmazon = array();
+
+        for ($index = 0; $index < count($isbns); $index = $index + 10) {
+
+            $isbnsSlice = array_slice($isbns, $index, 10);
+            \Sb\Trace\Trace::addItem(count($isbnsSlice) . " Isbns requetés par requête Amazon : " . implode(",", $isbnsSlice));
+
+            $amazonService = new Zend_Service_Amazon($config->getAmazonApiKey(), 'FR', $config->getAmazonSecretKey());
+            $responsesGroups = 'Small,ItemAttributes,Images,EditorialReview,Reviews';
+
+            // Recherche d'une liste de livre
+            $amazonResults = $amazonService->itemLookup(implode(",", $isbnsSlice),
+                array('SearchIndex' => 'Books',
+                    'AssociateTag' => $config->getAmazonAssociateTag(),
+                    'ResponseGroup' => $responsesGroups,
+                    'IdType' => 'ISBN'));
+
+            if ($amazonResults) {
+                $i = 0;
+                foreach ($amazonResults as $amazonResult) {
+                    $result = new \Sb\Db\Model\Book();
+                    \Sb\Db\Mapping\BookMapper::mapFromAmazonResult($result, $amazonResult);
+                    if ($result->IsValid()) {
+                        $i++;
+                        $booksFromAmazon[] = $result;
+                    }
+                    //var_dump($booksFromAmazon);
+                }
+                \Sb\Trace\Trace::addItem($i . " trouvés lors de cette requête amazon.");
+            }
+        }
+
+        return $booksFromAmazon;
+    }
+
+    private function addUserBook($bookId, \Sb\Context\Model\Context $context) {
+
+        $userBook = new \Sb\Db\Model\UserBook();
+        $userBook->setUserId($context->getConnectedUser()->getId());
+        $userBook->setBookId($bookId);
+        $userBook->setIsOwned(true);
+        $userBookDao = \Sb\Db\Dao\UserBookDao::getInstance();
+        $returnId = $userBookDao->Add($userBook);
+        if ($returnId) {
+            \Sb\Trace\Trace::addItem("Le livre a été ajouté à la biblio correctement.");
+        } else {
+            \Sb\Trace\Trace::addItem("KO : Le livre n'a pas été ajouté à la biblio.");
+        }
+        return $returnId;
+    }
+
+    private function showTableSummary($title, $books, \Sb\Context\Model\Context $context) {
+
+        $tplRows = array();
+        $idx = 0;
+        foreach ($books as $book) {
+            $idx++;
+            $tplRow = new \Sb\Templates\Template("import/tableRow");
+            $tplRow->set("isbn", $book->getISBN10());
+            $tplRow->set("title", $book->getTitle());
+            $tplRow->set("index", $idx);
+            $tplRows[] = $tplRow;
+        }
+        $rows = \Sb\Templates\Template::merge($tplRows);
+
+        $tpl = new \Sb\Templates\Template("import/table");
+        $tpl->set("rows", $rows);
+        $tpl->set("tableTitle", $title);
+        return $tpl->output();
+    }
+
+    private function showNotImported($isbnsNotImported, \Sb\Context\Model\Context $context) {
+        $tpl = new \Sb\Templates\Template("import/notImported");
+        $tpl->set("isbns", implode(", ", $isbnsNotImported));
+        $tpl->set("nbBooks", count($isbnsNotImported));
+        return $tpl->output();
+    }
+
+    // End Function for import action
 }
--- library/Sb/Entity/LibraryPages.php
+++ library/Sb/Entity/LibraryPages.php
@@ -7,12 +7,9 @@ namespace Sb\Entity;
  * @author Didier
  */
 class LibraryPages {
-    
-    //
+
     const LENDING_BORROW = "lending/borrow";
     const LENDING_BORROWFROMFRIENDS = "lending/borrowfromfriends";
     const LENDING_EDIT = "lending/edit";
     const LENDING_VALIDATE = "lending/validate";
-    //
-    const USERBOOK_IMPORT = "userbook/import";
 }
